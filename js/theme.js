9da0f33e0c7604c47bb3b295bd151a5bd5f9b67a
Adrian Larenaudie <alarenaudie@zaion.ai>
2023-03-21 17:55:40 +0100

    add city theme

--- js/theme.js
+++ js/theme.js
@@ -4,22 +4,55 @@ const allTags = document.querySelectorAll('*');
 
 const retro_theme_switcher = document.querySelector('.retro_theme_switcher');
 const oceanic_theme_switcher = document.querySelector('.oceanic_theme_switcher');
+const city_theme_switcher = document.querySelector('.city_theme_switcher');
 
 let currentTheme = 'retro';
 //const currentTheme = 'oceanic';
+city_theme_switcher.addEventListener('click', (event) => {
+    if(currentTheme !== 'city') {
+        currentTheme = 'city';
+        event.target.classList.add('city_theme_selector_active');
+        application.classList.add('city_theme_main_background');
+        application.classList.remove('oceanic_theme_main_background', 'retro_theme_main_background');
+        menu_mobile.classList.add('city_theme_main_background');
+        menu_mobile.classList.remove('oceanic_theme_main_background', 'retro_theme_main_background');
+        
+        allTags.forEach(tag => {
+            try {
+                tag.classList.remove('oceanic_theme_main', 'oceanic_theme_link', 'oceanic_theme_word', 'oceanic_theme_background_text', 'oceanic_theme_selector_active', 'oceanic_theme_svg', 'retro_theme_svg', 'retro_theme_main', 'retro_theme_link', 'retro_theme_word', 'retro_theme_background_text', 'retro_theme_selector_active', 'retro_theme_svg');
+                if(tag.className.includes('solo_word')) {
+                    tag.classList.add('city_theme_word');
+                } else if (tag.className.includes('background_word')) {
+                    tag.classList.add('city_theme_background_text');
+                } else if (tag.className.includes('link')) {
+                    tag.classList.add('city_theme_link');
+                } else if (tag.className.includes('social_network')) {
+                    tag.classList.add('city_theme_svg');
+                } else if (tag.className.includes('footer_segment') || tag.className.includes('burger_span') || tag.className.includes('menu_mobile_segment')) {
+                    tag.classList.add('city_theme_selector_active')
+                } else {
+                    tag.classList.add('city_theme_main');
+                }
+            } catch (error) {
+                if(!error.TYpeError === 'tag.className.includes is not a function')
+                    console.log(error);
+            }
+        });
+    }
+});
 
 retro_theme_switcher.addEventListener('click', (event) => {
     if(currentTheme !== 'retro') {
         currentTheme = 'retro';
         event.target.classList.add('retro_theme_selector_active');
         application.classList.add('retro_theme_main_background');
-        application.classList.remove('oceanic_theme_main_background');
+        application.classList.remove('oceanic_theme_main_background', 'city_theme_main_background');
         menu_mobile.classList.add('retro_theme_main_background');
-        menu_mobile.classList.remove('oceanic_theme_main_background');
+        menu_mobile.classList.remove('oceanic_theme_main_background', 'city_theme_main_background');
         
         allTags.forEach(tag => {
             try {
-                tag.classList.remove('oceanic_theme_main', 'oceanic_theme_link', 'oceanic_theme_word', 'oceanic_theme_background_text', 'oceanic_theme_selector_active', 'oceanic_theme_svg');
+                tag.classList.remove('oceanic_theme_main', 'oceanic_theme_link', 'oceanic_theme_word', 'oceanic_theme_background_text', 'oceanic_theme_selector_active', 'oceanic_theme_svg', 'city_theme_main', 'city_theme_link', 'city_theme_word', 'city_theme_background_text', 'city_theme_selector_active', 'city_theme_svg');
                 if(tag.className.includes('solo_word')) {
                     tag.classList.add('retro_theme_word');
                 } else if (tag.className.includes('background_word')) {
@@ -46,12 +79,12 @@ oceanic_theme_switcher.addEventListener('click', (event) => {
         currentTheme = 'oceanic'; 
         event.target.classList.add('oceanic_theme_selector_active');
         application.classList.add('oceanic_theme_main_background');
-        application.classList.remove('retro_theme_main_background');
+        application.classList.remove('retro_theme_main_background', 'city_theme_main_background');
         menu_mobile.classList.add('oceanic_theme_main_background');
-        menu_mobile.classList.remove('retro_theme_main_background');
+        menu_mobile.classList.remove('retro_theme_main_background', 'city_theme_main_background');
         allTags.forEach(tag => {
             try {
-                tag.classList.remove('retro_theme_main', 'retro_theme_link', 'retro_theme_word', 'retro_theme_background_text', 'retro_theme_selector_active', 'retro_theme_svg');
+                tag.classList.remove('retro_theme_main', 'retro_theme_link', 'retro_theme_word', 'retro_theme_background_text', 'retro_theme_selector_active', 'retro_theme_svg', 'city_theme_main', 'city_theme_link', 'city_theme_word', 'city_theme_background_text', 'city_theme_selector_active', 'city_theme_svg');
             if(tag.className.includes('solo_word')) {
                 tag.classList.add('oceanic_theme_word');
             } else if (tag.className.includes('background_word')) {
